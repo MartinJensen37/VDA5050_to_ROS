4eebba057deaaf4f7742156ce930b6727f17a7a7
martinjensen37 <martin.jensen37@gmail.com>
2022-02-15 14:53:34 +0100

    minor revisions

--- master_ctrl/master_controller.py
+++ master_ctrl/master_controller.py
@@ -1,6 +1,11 @@
-from master_ctrl_class import masterControl, actions, node
-import json
+# Standard python modules
 import sys
+
+# Master controller class module
+from master_ctrl_class import masterControl, actions, node
+
+# Other modules
+import json
 import uuid
 
 def state(client, userdata, message):
@@ -17,8 +22,8 @@ def main():
 
     # define order/node/action properties (edges will be added later)
     node_id_list = ['warehouse', 'packing', 'manualHandling', 'charger', 'palletizer']
-    sequence_ids = list(range(len(node_id_list)))
-    released_list = [True, True, True, False, False]
+    sequence_ids = [2, 3, 1, 5, 4] # Order in which the nodes have to be visited.
+    released_list = [True, True, True, False, False] # the corresponding release_list
     action_type_list = [['stopPause', 'wait'],['waitForTrigger'],['wait'],['cancelOrder'],['startPause']]
     blocking_type_list = [['HARD', 'HARD'],['HARD'],['HARD'],['SOFT'],['SOFT']]
 
@@ -42,12 +47,12 @@ def main():
     # if the message is valid send it.
     if is_valid == True:
         order_out = json.dumps(order_temp) # Convert JSON dict to JSON string that can later be loaded as a dict in the task manager
-        status =instance_mc.publish_order(order_out) # Publish the order
+        status = instance_mc.publish_order(order_out) # Publish the order
     
     # create an instance of the subscriber that can listen to the state of the robot
     instance_mc.subcriber()
     instance_mc.mqtt_client.on_message = state # define the callback function where the state is decoded and used
-    instance_mc.mqtt_client.loop_forever() # wait for new state messages until the process is closed
+    #instance_mc.mqtt_client.loop_forever() # wait for new state messages until the process is closed
 
 
 if __name__ == "__main__":
